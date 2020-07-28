5c860133625a560ea5dd5881c8c913868de4c209
Noah Alston <crashlive112@gmail.com>
2020-07-28 21:16:10 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -2,7 +2,7 @@
 #Greet the user.
 #Ask the user to input their name.
 #Capture and store the user's input using the #gets method.
-#Use that captured input to puts out a string that greets 
+#Use that captured input to puts out a string that greets
 #the user by name, using string interpolation.
 def greeting(name)
 
